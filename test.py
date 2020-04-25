3405cfe7a61f79fd4dd4af5b36762af01d67b930
hekun <hekun97@outlook.com>
2020-04-26 01:50:27 +0800

    add a line

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ def test():
     pager_obj = Pagination(request.args.get("page", 1), len(li), request.path, request.args, per_page_count=10)
     print(request.path)
     print(request.args)
+    print('this is a test')
     index_list = li[pager_obj.start:pager_obj.end]
     html = pager_obj.page_html()
     return render_template("test.html", index_list=index_list, html=html)
