ef9aaa48d8b6a42a9f8542b2606091ccee2743e6
hekun <hekun97@outlook.com>
2020-04-28 01:00:35 +0800

    some css and js

---
+++ App/static/js/now_time.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var time = new Date();
+    var day = ("0" + time.getDate()).slice(-2);
+    var month = ("0" + (time.getMonth() + 1)).slice(-2);
+    var today = time.getFullYear() + "-" + (month) + "-" + (day);
+    $('#date1').val(today);
+    $('#date2').val(today);
+})
