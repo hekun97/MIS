f8b74139c0c39b787101645016caeddee4cd1a4e
hekun <hekun97@outlook.com>
2020-04-28 00:58:33 +0800

    some style

--- App/static/js/leave_time.js
+++ App/static/js/leave_time.js
@@ -1,9 +1,9 @@
-function huoqu(date1,date2){
-	var a=document.getElementById("date1").value;
-	var b=document.getElementById("date2").value;
-	var date1=new Date(a);
-	var date2=new Date(b);
+function huoqu(date1, date2) {
+	var a = document.getElementById("date1").value;
+	var b = document.getElementById("date2").value;
+	var date1 = new Date(a);
+	var date2 = new Date(b);
 	var date3 = date2.getTime() - date1.getTime();
-	var days = Math.floor(date3 / (24 * 3600 * 1000)+1);
-	document.getElementById("days").value=days;
+	var days = Math.floor(date3 / (24 * 3600 * 1000) + 1);
+	document.getElementById("days").value = days;
 };
